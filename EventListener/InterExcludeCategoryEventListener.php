aad93be01bc0fc428ab43ffc58d43fbbb4575293
Etienne Perriere <eperriere@openstudio.fr>
2018-01-16 11:01:19 +0100

    Loop over product's parent categories + refacto

--- EventListener/InterExcludeCategoryEventListener.php
+++ EventListener/InterExcludeCategoryEventListener.php
@@ -6,6 +6,10 @@ use InterExcludeCategory\Model\InterExcludeCategoryQuery;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Thelia\Core\Event\Cart\CartEvent;
 use Thelia\Core\Event\TheliaEvents;
+use Thelia\Model\CartItem;
+use Thelia\Model\Category;
+use Thelia\Model\CategoryQuery;
+use Thelia\Model\Product;
 use Thelia\Model\ProductQuery;
 
 class InterExcludeCategoryEventListener implements EventSubscriberInterface
@@ -32,42 +36,88 @@ class InterExcludeCategoryEventListener implements EventSubscriberInterface
             null !== $productId &&
             null !== $product = ProductQuery::create()->findOneById($productId)
         ) {
-            // @TODO loop on children categories
-            // Get categories of cart's products
-            $cartProductsCategories = [];
+            $cartProductsCategories = $this->getCartItemsCategories($cartItems);
 
-            foreach ($cartItems as $cartItem) {
-                $cartItemCategories = $cartItem->getProduct()->getCategories();
+            $addedProductCategories = $this->getAddedProductCategories($product);
 
-                foreach ($cartItemCategories as $cartItemCategory) {
-                    $cartProductsCategories[] = $cartItemCategory->getId();
-                }
+            $this->compareCategories($cartProductsCategories, $addedProductCategories);
+        }
+    }
+    
+    private function getCartItemsCategories($cartItems)
+    {
+        $cartProductsCategories = [];
+
+        /** @var CartItem $cartItem */
+        foreach ($cartItems as $cartItem) {
+            $cartItemCategories = $cartItem->getProduct()->getCategories();
+
+            $cartProductsCategories = array_merge(
+                $cartProductsCategories,
+                $this->getProductCategories($cartItemCategories)
+            );
+        }
+
+        return array_unique($cartProductsCategories);
+    }
+
+    /**
+     * @param Product $product
+     * @return array
+     */
+    private function getAddedProductCategories($product)
+    {
+        $productCategories = $product->getCategories();
+
+        return array_unique($this->getProductCategories($productCategories));
+    }
+
+    /**
+     * @param $productCategories
+     * @return array
+     */
+    private function getProductCategories($productCategories)
+    {
+        $productCategoriesArray = [];
+
+        /** @var Category $category */
+        foreach ($productCategories as $category) {
+            $productCategoriesArray[] = $category->getId();
+            $parentCategoryId = $category->getParent();
+
+            while (0 != $parentCategoryId) {
+                $productCategoriesArray[] = $parentCategoryId;
+
+                $parentCategoryId = CategoryQuery::create()
+                    ->filterById($parentCategoryId)
+                    ->select('parent')
+                    ->findOne();
             }
+        }
 
-            // Get categories of the product being added to cart
-            $addedProductCategories = $product->getCategories();
+        return $productCategoriesArray;
+    }
 
-            foreach ($addedProductCategories as $addedProductCategory) {
-                $addedProductCategoryId = $addedProductCategory->getId();
+    private function compareCategories($cartProductsCategories, $addedProductCategories)
+    {
+        foreach ($cartProductsCategories as $cartProductCategoryId) {
+            foreach ($addedProductCategories as $addedProductCategoryId) {
+                $interExcludeCategory = InterExcludeCategoryQuery::create()
+                    ->filterByFirstCategoryId($cartProductCategoryId)
+                    ->filterBySecondCategoryId($addedProductCategoryId)
+                    ->find();
 
-                foreach ($cartProductsCategories as $cartProductCategoryId) {
+                // Check both first/second and second/first combinations
+                if (empty($interExcludeCategory->getData())) {
                     $interExcludeCategory = InterExcludeCategoryQuery::create()
-                        ->filterByFirstCategoryId($cartProductCategoryId)
-                        ->filterBySecondCategoryId($addedProductCategoryId)
+                        ->filterByFirstCategoryId($addedProductCategoryId)
+                        ->filterBySecondCategoryId($cartProductCategoryId)
                         ->find();
+                }
 
-                    // Check both first/second and second/first combinations
-                    if (empty($interExcludeCategory->getData())) {
-                        $interExcludeCategory = InterExcludeCategoryQuery::create()
-                            ->filterByFirstCategoryId($addedProductCategoryId)
-                            ->filterBySecondCategoryId($cartProductCategoryId)
-                            ->find();
-                    }
-
-                    if (!empty($interExcludeCategory->getData())) {
-                        // Exclusion found: prevent adding product to cart
-                        $a = 'ici';
-                    }
+                if (!empty($interExcludeCategory->getData())) {
+                    // Exclusion found: prevent adding product to cart
+                    $a = 'ici';
                 }
             }
         }
